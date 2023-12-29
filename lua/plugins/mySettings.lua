225f0874a53078c3091c65c28b1fd84e1f76424a
Wosk <krynio340@gmail.com>
2023-12-29 19:29:08 +0100

    moje ustawienia

---
+++ lua/plugins/mySettings.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            untracked = "",
+          },
+        },
+      },
+    },
+  },
+}
